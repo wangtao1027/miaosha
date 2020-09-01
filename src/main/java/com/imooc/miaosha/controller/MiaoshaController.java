171712528a3e1b2e29a02b09ef33af55c6160cee
wangtao <907813486@qq.com>
2020-09-01 17:32:55 +0800

    页面静态化

--- src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
+++ src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
@@ -4,6 +4,7 @@ import com.imooc.miaosha.domain.MiaoShaOrder;
 import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.domain.OrderInfo;
 import com.imooc.miaosha.result.CodeMsg;
+import com.imooc.miaosha.result.Result;
 import com.imooc.miaosha.service.GoodsService;
 import com.imooc.miaosha.service.MiaoshaService;
 import com.imooc.miaosha.service.OrderService;
@@ -32,31 +33,27 @@ public class MiaoshaController {
     private MiaoshaService miaoshaService;
 
     @RequestMapping("/do_miaosha")
-    public String list(Model model, MiaoshaUser user, @RequestParam("goodsId") Long goodsId) {
+    public Result<OrderInfo> list(Model model, MiaoshaUser user, @RequestParam("goodsId") Long goodsId) {
         logger.info("run method list");
         model.addAttribute("user",user);
         if (user == null) {
-            return "login";
+            return Result.error(CodeMsg.SESSION_ERROR);
         }
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         int stock = goods.getStockCount();
         //判断是否还有库存
         if (stock <= 0) {
-            model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
-            return "miaosha_fail";
+            return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
 
         //判断是否已经秒杀到了
         MiaoShaOrder miaoshsOrder = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
         if (miaoshsOrder != null) {
-            model.addAttribute("errmsg",CodeMsg.REPEATE_MIAOSHA.getMsg());
-            return "miaosha_fail";
+            return Result.error(CodeMsg.REPEATE_MIAOSHA);
         }
         //扣减库存,创建订单 写入秒杀订单
         OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
-        model.addAttribute("orderInfo",orderInfo);
-        model.addAttribute("goods",goods);
-        return "order_detail";
+        return Result.success(orderInfo);
     }
 
 }
